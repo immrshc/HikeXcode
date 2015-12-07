d1b461187d84750e748f55ede10fafab08f410ba
Shoichi Imamura <syoichiimamura@gmail.com>
2015-12-07 23:21:55 +0900

    カメラロールへのアクセスを実装

--- HikeXcode/PostController.swift
+++ HikeXcode/PostController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class PostController: UIViewController {
+class PostController: UIViewController, UITabBarDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    //var imageURL:String = "postBackImage.jpg"
     var latitude: Double?
     var longitude: Double?
 
@@ -20,9 +21,7 @@ class PostController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //PostクラスでlocationManagerが実行できるかのテスト
-        //Post(content: "", imageURL: "", latitude: 0, longitude: 0).locationUpdate()
-        //Postでは出来なくて、PostControllerでは出来るのはなぜか質問する
+        selectImageTB.delegate = self
         
         postBackIV.image = UIImage(named:"postBackImage.jpg")
 
@@ -36,6 +35,49 @@ class PostController: UIViewController {
                 self.longitude = longitude
                 print("ユーザの投稿時の緯度経度：\(latitude), \(longitude)")
         }
+        //PostクラスでlocationManagerが実行できるかのテスト
+        //Post(content: "", imageURL: "", latitude: 0, longitude: 0).locationUpdate()
+        //Postでは出来なくて、PostControllerでは出来るのはなぜか質問する
+    }
+    
+    //押されたタブをタグで識別して処理を分ける
+    func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem) {
+        switch item.tag {
+            case 0:
+                print(item.tag)
+            case 1:
+                print(item.tag)
+                //カメラロールへアクセス
+                self.pickImageFromLibrary()
+            default:
+                print(item.tag)
+        }
+    }
+    
+    //ライブラリから写真を選択する
+    func pickImageFromLibrary() {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+            let controller = UIImagePickerController()
+            controller.delegate = self
+            controller.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            self.presentViewController(controller, animated: true, completion: nil)
+        }
+    }
+    
+    //写真を選択した時に呼ばれる
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if info[UIImagePickerControllerOriginalImage] != nil {
+            
+            if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+                self.postBackIV.image = image
+            }
+            
+            if let assetURL:AnyObject = info[UIImagePickerControllerReferenceURL] {
+                print(String(assetURL))
+                //このURLを取り出すにはAssetLibrary.Frameworkが必要になる
+            }
+            picker.dismissViewControllerAnimated(true, completion: nil)
+        }
     }
     
     @IBAction func cancelPost(sender: AnyObject) {
@@ -45,6 +87,7 @@ class PostController: UIViewController {
     @IBAction func doPost(sender: AnyObject) {
         if let text:String = self.postShowTV.text,
             //Xcode内の画像のURLを引き渡す
+            //画像がカメラロールから選ばれた場合は、AssetLibrary.Frameworkでカメラロールへのパスを取得してimageURLに格納する
             let imageURL:String = "postBackImage.jpg",
             let latitude:Double = self.latitude,
             let longitude:Double = self.longitude {
